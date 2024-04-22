3963d2c33fe0ecfed27347c4ea13ba180e7ac0b7
Hatashu <78066093+Hatashu@users.noreply.github.com>
2024-04-22 21:33:30 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,124 @@
+// 問題
+const question = [
+    [8, 7, 1, 0, 0, 0, 5, 6, 4],
+    [0, 9, 5, 0, 1, 7, 2, 3, 8],
+    [2, 0, 3, 4, 5, 8, 0, 7, 1],
+    [0, 2, 0, 1, 0, 3, 7, 9, 5],
+    [0, 1, 9, 2, 7, 0, 8, 4, 3],
+    [7, 0, 4, 0, 8, 5, 0, 0, 2],
+    [1, 5, 0, 0, 0, 4, 3, 8, 0],
+    [0, 8, 7, 5, 0, 0, 0, 0, 6],
+    [0, 0, 0, 0, 3, 2, 1, 0, 7],
+  ];
+
+  let character=[1,2,3,4,5,6,7,8,9];
+  let select_charcter=0;
+  let selected_val = document.getElementById("school");
+
+  function buttonclick(){
+    selected_val = document.getElementById("school");
+    console.log(selected_val.value);
+    if(selected_val.value=="Nijigasaki"){
+      const checkboxes=document.querySelectorAll('input[name="checkItem"]:checked');
+
+      for(let i=0;i<9;i++){
+        character[i]=checkboxes[i].value;
+      }
+    }
+  
+  // クリックされた要素を保持
+  let place;
+  
+  init();
+  // ゲーム画面生成
+  function init() {
+    
+    const main = document.querySelector(".main");
+    const select = document.querySelector(".select");
+    
+    for (let i = 0; i < 9; i++) {
+      let tr = document.createElement("tr");
+      for (let j = 0; j < 9; j++) {
+        let td = document.createElement("td");
+        td.onclick = mainClick;
+        tr.appendChild(td);
+        if (question[i][j] != 0) {
+            td.classList.add("clickdisable");
+            td.textContent = question[i][j];
+            let img=document.createElement("img");
+            img.src=(selected_val.value+character[question[i][j]-1]+".png");
+            td.appendChild(img);
+        } else {
+          td.textContent = null;
+          td.classList.add("clickenable");
+        }
+      }
+      main.appendChild(tr);
+    }
+  
+    for (let i = 0; i < 9; i++) {
+      let td = document.createElement("td");
+      td.onclick = selectClick;
+      td.value = i + 1;
+      select.appendChild(td);
+      td.textContent = i + 1;
+      let img=document.createElement("img");
+      img.src=(selected_val.value+character[i]+".png");
+      td.appendChild(img);
+    }
+  }
+  
+  // 問題パネルのマスが押された時の処理
+  function mainClick(e) {
+    if (place != undefined) {
+      place.classList.remove("mainClick");
+    }
+  
+    place = e.target;
+    place.classList.add("mainClick");
+  }
+  
+  // 数字選択のマスが押された時の処理
+  function selectClick(e) {
+    place.textContent = e.currentTarget.value;
+    let img=document.createElement("img");
+    img.src=(selected_val.value+character[e.target.value-1]+".png");
+    place.appendChild(img);
+  }
+  
+  // 正解判定
+  function check() {
+    const h2 = document.querySelector("h2");
+    const tr = document.querySelectorAll(".main tr");
+    let checkFlag = true;
+    // 横計算
+    for (let i = 0; i < 9; i++) {
+      let sum = 0;
+      let td = tr[i].querySelectorAll("td");
+      for (let j = 0; j < 9; j++) {
+        sum += Number(td[j].textContent);
+      }
+      if (sum != 45) {
+        checkFlag = false;
+        break;
+      }
+    }
+    // 縦計算
+    for (let i = 0; i < 9; i++) {
+      let sum = 0;
+      for (let j = 0; j < 9; j++) {
+        let td = tr[j].querySelectorAll("td");
+        sum += Number(td[i].textContent);
+      }
+      if (sum != 45) {
+        checkFlag = false;
+        break;
+      }
+    }
+    if (checkFlag) {
+      h2.textContent = "正解です!!";
+    } else {
+      h2.textContent = "間違いがあります";
+    }
+  }
+}
